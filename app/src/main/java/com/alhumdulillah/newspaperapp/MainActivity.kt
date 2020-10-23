dcafe6aa00f05e21db01e7f21f3f4def086f2c6c
Muhammad Al Mamun <68417760+AlMamun-CSE@users.noreply.github.com>
2020-10-23 21:10:57 +0600

    Update MainActivity.kt
    Comment Up My MainActivity Class

--- app/src/main/java/com/alhumdulillah/newspaperapp/MainActivity.kt
+++ app/src/main/java/com/alhumdulillah/newspaperapp/MainActivity.kt
@@ -10,16 +10,19 @@ class MainActivity : AppCompatActivity(), NewsItemClick {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
 
 
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         val items = fetchData()
         val adapter = NewsAdapter(items,this)
+        //Set Adapter This RecyclerView
         recyclerView.adapter = adapter
 
     }
 
+    //fetch Api Data for Show Data
     private fun fetchData():ArrayList<String>{
         val list  = ArrayList<String>()
         for (i in 0 until 100){
@@ -29,6 +32,7 @@ class MainActivity : AppCompatActivity(), NewsItemClick {
 
     }
 
+    //On Item Click Listener this recycler View Item
     override fun onItemClick(item: String) {
         Toast.makeText(this,"Item Is Clicked: $item ",Toast.LENGTH_LONG).show()
     }
